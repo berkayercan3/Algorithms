817857b16944583075fcdf2570ae1fd62f1e0aca
Berkay Ercan <115825102+berkayercan3@users.noreply.github.com>
2025-06-12 14:07:32 +0300

    DivideAndConquerAlgorithms and AsalBul and FindKlargest

---
+++ src/DİvideAndConquerAlgorithms.java
@@ -0,0 +1,18 @@
+public class DİvideAndConquerSumArray {
+}
+
+/*
+SumArray(A[] , sol, sag){
+    orta = (sol + sag) / 2
+    if sol == sag
+        return sol
+
+    solToplam = SumArray(A[] ,sol, orta)
+    sagToplam = SumArray(A[] ,orta + 1 , sag)
+
+    return solToplam + sagToplam
+
+
+
+}
+*/
